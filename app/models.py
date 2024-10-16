251b6a130e1db7b58227120012c45fb38e1ab866
Renato Nascimento <joaoseraficonascimento@gmail.com>
2024-10-16 17:23:10 -0300

    Criação do aplicativo e do modelo DadosHidricos

---
+++ app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class DadosHidricos(models.Model):
+    cidade = models.CharField(null = False, max_length = 100)
+    ind_pluviometricos = models.DecimalField(null = False, max_digits = 10, decimal_places = 4)
+    bal_hidrico = models.DecimalField(null = False, max_digits = 10, decimal_places = 4)
+    precipitacao = models.DecimalField(null = False, max_digits = 10, decimal_places = 4)
+    
