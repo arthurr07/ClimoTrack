3d3ee8a00302099069e7ce24cb2b940c2eae93cb
Guilherme Silva <guilp490@gmial.com>
2024-10-16 17:54:46 -0300

    Adicionado modelo DadosHidricosList a area de administrador do projeto

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from app.models import DadosHidricos
 
+class DadosHidricosList(admin.ModelAdmin):
+    list_display = ('cidade', 'bal_hidrico', 'ind_pluviometricos', 'precipitacao')
+    list_display_links = ('cidade', 'bal_hidrico', 'ind_pluviometricos', 'precipitacao')
+    search_fields = ('cidade', 'bal_hidrico')
+
+
+
+
+
+admin.site.register(DadosHidricos, DadosHidricosList)
 # Register your models here.
