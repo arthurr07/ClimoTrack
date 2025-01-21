b55a1a6ec008560cda65fac6170d76711f4efa75
Guilherme <guilp490@gmail.com>
2025-01-21 17:54:21 -0300

    Foram substituidos os botões de Ajuda e Contato pelo botão Equipe. Além disso, foi solucionado o problema das imagens que não estavam aparecendo

--- script.py
+++ script.py
@@ -3,6 +3,6 @@ import sqlite3
 
 df = pd.read_csv("banco/BASE FINAL - DADOS CLIMÁTICOS - Cópia.xlsm - Plan1.csv", delimiter = ",", on_bad_lines="skip", encoding='ISO-8859-1')
 df.columns = df.columns.str.strip()
-connection =sqlite3.connect("sqlite3.db")
+connection =sqlite3.connect("db.sqlite3")
 df.to_sql('table', connection, if_exists='replace')
 print(df)
