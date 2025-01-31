b54f06824ea1e057978233b9c8cbb7277a64f956
Renato <joaoseraficonascimento@gmail.com>
2025-01-31 16:16:17 -0300

    Rodapé fixado e tentativa de exibir os dados na tela do site

--- app/views.py
+++ app/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from app.models import Cidade
+from app.models import Cidade, DadosHidricos, Estado
 def index(request):
-    list = Cidade.objects.all()
-    return render(request, 'index.html', {'list':list})
-
+    city_list = Cidade.objects.all()
+    uf_list = Estado.objects.all()
+    if request.method == "POST":
+        cidade = int(request.POST['cidade'])
+        data = DadosHidricos.objects.select_related().filter(estacao=cidade)
+        return render(request, 'index.html', {'city_list':city_list, 'uf_list':uf_list, 'data':data})
+    return render(request, 'index.html', {'city_list':city_list, 'uf_list':uf_list})
+    
 def table(request): 
     city = request.GET.get('cidade')
     #list = Precipitacao.objects.all().filter(cidade=city)
