97d28c4b2eed31bba91a3e1c8d7cee8b031f8715
Guilherme <guilp490@gmail.com>
2025-02-13 18:02:10 -0300

    Foi acrescentado as linhas de médias na tabela do index

--- app/views.py
+++ app/views.py
@@ -8,6 +8,7 @@ def index(request):
         cidade = int(request.POST['cidade'])
         data = DadosHidricos.objects.filter(estacao=cidade).first()
         cidade_selecionada = Cidade.objects.get(id=cidade)
+        total = 0
         
         if data:  # Se houver dados para a cidade
             multiplicacao = {
@@ -23,16 +24,37 @@ def index(request):
                 'out': 2 * float(data.t_med_out),
                 'nov': 2 * float(data.t_med_nov),
                 'dez': 2 * float(data.t_med_dez),
+                'med_temp': (data.t_med_jan + data.t_med_fev + data.t_med_mar + data.t_med_abr + data.t_med_mai + data.t_med_jun + data.t_med_jul + data.t_med_ago + data.t_med_set + data.t_med_out 
+                             + data.t_med_nov + data.t_med_dez)/12,
+                'med_precip': (data.precip_md_jan + data.precip_md_fev + data.precip_md_mar + data.precip_md_abr + data.precip_md_mai + data.precip_md_jun + data.precip_md_jul + data.precip_md_ago + data.precip_md_set + data.precip_md_out 
+                             + data.precip_md_nov + data.precip_md_dez)/12,
             }
+            if  data.precip_md_jan <= multiplicacao.get('jan') : total = total+1
+            if  data.precip_md_fev <= multiplicacao.get('fev') : total = total+1
+            if  data.precip_md_mar <= multiplicacao.get('mar') : total = total+1
+            if  data.precip_md_abr <= multiplicacao.get('abr') : total = total+1
+            if  data.precip_md_mai <= multiplicacao.get('mai') : total = total+1
+            if  data.precip_md_jun <= multiplicacao.get('jun') : total = total+1
+            if  data.precip_md_jul <= multiplicacao.get('jul') : total = total+1
+            if  data.precip_md_ago <= multiplicacao.get('ago') : total = total+1
+            if  data.precip_md_set <= multiplicacao.get('set') : total = total+1
+            if  data.precip_md_out <= multiplicacao.get('out') : total = total+1
+            if  data.precip_md_nov <= multiplicacao.get('nov') : total = total+1
+            if  data.precip_md_dez <= multiplicacao.get('dez') : total = total+1
+            
         else:
             multiplicacao = {}
+        
+            
 
         return render(request, 'index.html', {
             'city_list': city_list,
             'uf_list': uf_list,
             'data': data,
             'multiplicacao': multiplicacao,
-            'cidade_selecionada': cidade_selecionada# Passa o dicionário com a multiplicação de cada mês
+            'cidade_selecionada': cidade_selecionada,
+            'total':total
+            # Passa o dicionário com a multiplicação de cada mês
         })
     
     return render(request, 'index.html', {'city_list': city_list, 'uf_list': uf_list})
