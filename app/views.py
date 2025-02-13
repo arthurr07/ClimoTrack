2b67acf1e9c9e35973a5c0ab00d0f5232806ea7a
Guilherme <guilp490@gmail.com>
2025-02-13 14:33:37 -0300

    Tabela foi inserida na página, mas agora está conseguindo trazer o valores solicitados para cada cidade; Banco de dados foi alterado, os numeros que apresentavam virgulas em sua estrutura foram transcrevidos, substituindo a virgula por pontos; O caption da tabela agora esta pegando o valor que foi selecionado dentro da caixa de select Cidade

--- app/views.py
+++ app/views.py
@@ -3,11 +3,39 @@ from app.models import Cidade, DadosHidricos, Estado
 def index(request):
     city_list = Cidade.objects.all()
     uf_list = Estado.objects.all()
+
     if request.method == "POST":
         cidade = int(request.POST['cidade'])
-        data = DadosHidricos.objects.select_related().filter(estacao=cidade)
-        return render(request, 'index.html', {'city_list':city_list, 'uf_list':uf_list, 'data':data})
-    return render(request, 'index.html', {'city_list':city_list, 'uf_list':uf_list})
+        data = DadosHidricos.objects.filter(estacao=cidade).first()
+        cidade_selecionada = Cidade.objects.get(id=cidade)
+        
+        if data:  # Se houver dados para a cidade
+            multiplicacao = {
+                'jan': 2 * float(data.t_med_jan),
+                'fev': 2 * float(data.t_med_fev),
+                'mar': 2 * float(data.t_med_mar),
+                'abr': 2 * float(data.t_med_abr),
+                'mai': 2 * float(data.t_med_mai),
+                'jun': 2 * float(data.t_med_jun),
+                'jul': 2 * float(data.t_med_jul),
+                'ago': 2 * float(data.t_med_ago),
+                'set': 2 * float(data.t_med_set),
+                'out': 2 * float(data.t_med_out),
+                'nov': 2 * float(data.t_med_nov),
+                'dez': 2 * float(data.t_med_dez),
+            }
+        else:
+            multiplicacao = {}
+
+        return render(request, 'index.html', {
+            'city_list': city_list,
+            'uf_list': uf_list,
+            'data': data,
+            'multiplicacao': multiplicacao,
+            'cidade_selecionada': cidade_selecionada# Passa o dicionário com a multiplicação de cada mês
+        })
+    
+    return render(request, 'index.html', {'city_list': city_list, 'uf_list': uf_list})
     
 def table(request): 
     city = request.GET.get('cidade')
