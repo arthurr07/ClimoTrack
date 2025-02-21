6d8a662206bb48d1b076077e5ffdc251069510af
Guilherme <guilp490@gmail.com>
2025-02-21 14:42:29 -0300

    Os nome dos campos das estações/cidades foi modificado; Foram modificados também as escalas que determinam o subtipo climático de uma determinada região, no index.

--- scripts/popularEstacoes.py
+++ scripts/popularEstacoes.py
@@ -2,7 +2,7 @@ import pandas as pd
 import sqlite3
 
 fields = ['ESTACAO']
-df = pd.read_csv("banco/BASE FINAL - DADOS CLIMÁTICOS - Cópia.xlsm - Plan1.csv", delimiter = ",", on_bad_lines="skip", encoding='ISO-8859-1', usecols=fields)
+df = pd.read_csv("banco/BASE FINAL - DADOS CLIMÁTICOS - Cópia.xlsm - Plan1.csv", delimiter = ",", on_bad_lines="skip", encoding='UTF-8', usecols=fields)
 df["estado_id"]=1
 df.columns = df.columns.str.strip()
 
